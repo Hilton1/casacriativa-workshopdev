a1a8c1e256c6c294c4207d5fa9fce778b1ab1c58
Hilton1 <josehilton02@gmail.com>
2020-04-25 00:19:28 -0300

    Adicionando backend e organizando arquivos.

---
+++ server.js
@@ -0,0 +1,83 @@
+// usei o express para criar e configurar meu servidor
+const express = require("express")
+const server = express()
+
+const ideas = [
+    {
+        img: "https://image.flaticon.com/icons/svg/2729/2729007.svg",
+        title: "Cursos de Programação",
+        category: "Estudo",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+        url: "https://rocketseat.com.br"
+    },
+    {
+        img: "https://image.flaticon.com/icons/svg/2729/2729005.svg",
+        title: "Exercícios",
+        category: "Saúde",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+        url: "https://rocketseat.com.br"
+    },
+    {
+        img: "https://image.flaticon.com/icons/svg/2729/2729027.svg",
+        title: "Meditação",
+        category: "Mentalidade",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+        url: "https://rocketseat.com.br"
+    },
+    {
+        img: "https://image.flaticon.com/icons/svg/2729/2729032.svg",
+        title: "Karaokê",
+        category: "Diversão em família",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+        url: "https://rocketseat.com.br"
+    },
+    {
+        img: "https://image.flaticon.com/icons/svg/2729/2729038.svg",
+        title: "Pintura",
+        category: "Criatividade",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+        url: "https://rocketseat.com.br"
+    },
+    {
+        img: "https://image.flaticon.com/icons/svg/2729/2729048.svg",
+        title: "Recortes",
+        category: "Criatividade",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+        url: "https://rocketseat.com.br"
+    },
+]
+
+
+// Configurar arquivos estáticos (css, scripts, imagens)
+server.use(express.static("public"))
+
+
+// Configuração do nunjucks
+const nunjucks = require("nunjucks")
+nunjucks.configure("views", {
+    express: server,
+    noCache: true,
+})
+
+// criei uma rota /
+// e capturo o pedido do cliente para responder
+server.get("/", function(require, response) {
+
+    const reversedIdeas = [...ideas].reverse()
+
+    let lestIdeas = []
+    for (let idea of reversedIdeas) {
+        if (lestIdeas.length < 2) {
+            lestIdeas.push(idea)
+        }
+    }
+    return response.render("index.html", { ideas: lestIdeas })
+})
+
+server.get("/ideias", function(require, response) {
+    const reversedIdeas = [...ideas].reverse()
+    return response.render("ideias.html", { ideas: reversedIdeas })
+})
+
+// Liguei meu servidor na porta 3000
+server.listen(3000);
